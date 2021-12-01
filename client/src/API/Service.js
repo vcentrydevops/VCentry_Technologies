1e6af0fee711418dd7572ca2a300d25bdd4ad984
harithilagar <harishthilagar06@gmail.m>
2021-12-01 16:43:15 +0530

    aws deploy

--- client/src/API/Service.js
+++ client/src/API/Service.js
@@ -2,10 +2,15 @@ import axios from 'axios'
 
 let baseUrl
 if (process.env.NODE_ENV === "production") {
-    baseUrl = `${window.location.origin}:5000/`
+    if(process.env.PORT){
+        baseUrl = `${window.location.origin}:${process.env.PORT}/`
+    }else{
+        baseUrl = `${window.location.origin}:5000/`
+    }   
 }else{
     baseUrl = "http://localhost:5000/"
 }
+
 class API {
 
     postApi(url, data, headers) {
